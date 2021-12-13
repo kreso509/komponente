7966c4aa77114a5eaaffdfb8fc1e3c1fda46711b
Kreso <kreso.m91@gmail.com>
2021-12-13 11:30:11 +0100

    App init

---
+++ src/WelcomeClass.js
@@ -0,0 +1,6 @@
+import React from 'react';
+export default class WelcomeClass extends React.Component {
+    render() {
+        return <h1>Komponenta definirana klasom</h1>;
+    }
+}
---
+++ src/WelcomeFunction.js
@@ -0,0 +1,3 @@
+export default function WelcomeFunction() {
+    return <h2>Komponenta definirana funkcijom</h2>
+}
